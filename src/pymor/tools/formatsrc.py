8a05b85b78792db7e31f08d91dfec978529c0e84
Petar Mlinarić <mlinaric@mpi-magdeburg.mpg.de>
2021-02-15 13:37:30 +0100

    [tools.formatsrc] add docstrings and fix flake8 issues

--- src/pymor/tools/formatsrc.py
+++ src/pymor/tools/formatsrc.py
@@ -8,10 +8,23 @@ from pymor.core.config import is_jupyter
 
 
 def format_source(obj):
+    """Format source code of an object.
+
+    Parameters
+    ----------
+    obj
+        The object of which to format the source code.
+
+    Returns
+    -------
+    source
+        The source code as a `Code` widget in case a Jupyter notebook is used or as a string
+        otherwise.
+    """
     source = getsource(obj)
 
     if is_jupyter():
-        from IPython.display import display, Code
+        from IPython.display import Code
         return Code(source, language='python')
     else:
         try:
@@ -24,9 +37,16 @@ def format_source(obj):
 
 
 def print_source(obj):
+    """Print source code of an object.
+
+    Parameters
+    ----------
+    obj
+        The object of which to print the source code.
+    """
     source = format_source(obj)
     if is_jupyter():
-        from IPython.display import display, Code
+        from IPython.display import display
         display(source)
     else:
         print(source)
