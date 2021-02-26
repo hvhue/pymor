f9e8e123f8b3c2ae8524822ec5b8d7806010ad8f
René Fritze <rene.fritze@wwu.de>
2021-02-26 16:01:11 +0100

    [docs] first docstring lines cannot end with rst literal notation

--- src/pymor/analyticalproblems/functions.py
+++ src/pymor/analyticalproblems/functions.py
@@ -131,7 +131,9 @@ class Function(ParametricObject):
 
 
 class ConstantFunction(Function):
-    """A constant |Function| ::
+    """A constant |Function|
+
+    Defined as ::
 
         f: R^d -> R^shape(c), f(x) = c
 
