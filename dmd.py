3d160024737269ae00f11d07afd337f8e0e2d480
hvhue <80212782+hvhue@users.noreply.github.com>
2021-03-07 17:52:01 +0100

    Add files via upload
    Implemented Dynamic Mode Decomposition

---
+++ dmd.py
@@ -0,0 +1,277 @@
+import numpy as np
+import scipy.linalg as spla
+
+from pymor.core.defaults import defaults
+from pymor.vectorarrays.interface import VectorArray
+from pymor.vectorarrays.numpy import NumpyVectorSpace
+from pymor.algorithms.svd_va import method_of_snapshots, qr_svd
+from pymor.algorithms.gram_schmidt import gram_schmidt
+
+from pymor.operators.constructions import VectorArrayOperator
+from pymor.algorithms.genericsolvers import lsqr
+
+
+@defaults('svd_method')
+def dmd(A=None, XL=None, XR=None, target_rank=None, dt=1, modes='exact', order=True, svd_method='qr_svd'):
+    """Dynamic Mode Decomposition.
+
+    See Algorithm 1 and Algorithm 2 in [TRLBK14]_.
+
+    Parameters
+    ----------
+    A  :  optional
+        The |VectorArray| for which the DMD Modes are to be computed.
+        If A is used, XL and XR should not be used.
+
+    XL  :  optional
+        The |VectorArray| of the left Snapshot series A[:-1].
+    XR  :  optional
+        The |VectorArray| of the right Snapshot series A[1:].
+        If XL and XR are given, A should not be given.
+
+    target_rank : int
+        Number of DMD Modes to be computed. If None target_rank = len(A).
+
+    dt : scalar, optional (default: 1)
+        Factor specifying the time difference between the observations.
+        Used if the input data is a timeseries in continuous time.
+
+    modes : str `{'standard', 'exact', 'exact_scaled'}`
+        - 'standard' : uses the standard definition to compute the dynamic modes, `Wk = U * EVECS`.
+        - 'exact' : computes the exact dynamic modes, `Wk = XR * V * Sigma_inv * EVECS`.
+        - 'exact_scaled' : computes the exact dynamic modes,
+                           `Wk = (1/EVALS) * XR * V * Sigma_inv * EVECS`.
+
+    order : bool `{True, False}`
+        True: return modes sorted.
+
+    svd_method
+        Which SVD method from :mod:`~pymor.algorithms.svd_va` to use
+        (`'method_of_snapshots'` or `'qr_svd'`).
+
+
+    Returns
+    -------
+    Wk : VectorArray
+        |VectorArray| containing the dynamic modes.
+
+    omega : array_like
+        Time scaled eigenvalues: `ln(l)/dt`.
+    """
+
+    assert modes in ('exact', 'standard', 'exact_scaled')
+    assert A is None or (XL is None and XR is None)
+    assert not(A is None and (XL and XR is None))
+    assert isinstance(A, VectorArray) or A is None
+    assert isinstance(XL, VectorArray) or XL is None
+    assert isinstance(XR, VectorArray) or XR is None
+    assert A is None or (target_rank is None or target_rank <= len(A))
+    assert XL is None or (target_rank is None or target_rank < len(XL))
+    assert XL is None and XR is None or len(XL) == len(XR)
+    assert order in (True, False)
+    assert svd_method in ('qr_svd', 'method_of_snapshots')
+
+    # XL = x0, ..., xm-1  ; XR = x1, ..., xm
+    if A is not None:
+        XL = A[:-1]
+        XR = A[1:]
+
+    rank = len(XL) if target_rank is None else target_rank
+    svd = qr_svd if svd_method == 'qr_svd' else method_of_snapshots
+
+    print('SVD of XL...')
+    U, SVALS, Vh = svd(XL, product=None, modes=rank)
+
+    # Invert the Singular Values
+    SVALS_diag = np.diag(SVALS)
+    Sigma_inv = np.linalg.inv(SVALS_diag)
+    V = Vh.conj().T
+
+    # Solve the least Sqaures Problem
+    # real: A_tilde = U.T * XR * Vh.T * Sigma_inv
+    # complex: A_tilde = U.H * XR * Vh.H * Sigma_inv
+    # A = XR[:k] @ V @ Sigma_inv.inner(Uh)
+    A_tilde = U.inner(XR) @ V @ Sigma_inv
+
+    print('Calculating eigenvalue dec. ...')
+    EVALS, EVECS = spla.eig(A_tilde, b=None, left=False, right=True)
+    # omega = np.log(EVALS) / dt
+
+    omega = EVALS / dt
+
+    if order:
+        print('Sorting...')
+        # return ordered result
+        sort_idx = np.argsort(np.abs(omega))[::-1]
+        EVECS = EVECS[:, sort_idx]
+        EVALS = EVALS[sort_idx]
+        omega = omega[sort_idx]
+
+    print('Reconstructing Eigenvectors...')
+    if modes == 'standard':
+        Wk = U.lincomb(EVECS.T)
+    elif modes == 'exact' or 'exact_scaled':
+        Wk = XR.lincomb(V.T).lincomb(Sigma_inv.T).lincomb(EVECS.T)
+        if modes == 'exact_scaled':
+            EVALS_inv = np.reciprocal(EVALS)
+            Wk = Wk*EVALS_inv
+
+    return Wk, omega
+
+
+def rand_QB(A, target_rank=None, distribution='normal', oversampling=0, powerIterations=0):
+    """
+    randomisierte QB-Zerlegung
+
+    See Algorithm 3.1 in [EMKB19]_.
+
+    Parameters
+    ----------
+    A  :
+        The |VectorArray| for which the randomized QB Decomposition is to be computed.
+
+    target_rank  :  int
+        The desired rank for the decomposition. If None rank = len(A).
+
+    distribution : str
+        Distribution used for the random projectionmatrix Omega. (`'normal'` or `'uniform'`)
+
+    oversampling : int
+        Oversamplingparameter. Number of extra columns of the projectionmatrix.
+
+    powerIterations : int
+        Number of power Iterations.
+
+
+    Returns
+    -------
+    Q :
+        |VectorArray| containig an approximate optimal Basis for the Image of the Inputmatrix A.
+        len(Q) = target_rank
+    B :
+        Numpy Array. Projection of the Input Matrix into the lower dimensional subspace.
+    """
+    assert isinstance(A, VectorArray)
+    assert target_rank is None or target_rank <= len(A)
+    assert distribution in ('normal', 'uniform')
+
+    if A.dim == 0 or len(A) == 0:
+        return A.space.zeros(), np.zeros((target_rank, len(A)))
+
+    rank = len(A) if target_rank is None else target_rank + oversampling
+    target_rank = len(A) if target_rank is None else target_rank
+
+    Omega = np.random.normal(0, 1, (rank, len(A))) if distribution == 'normal' else np.random.rand(rank, len(A))
+
+    Y = A.lincomb(Omega)[:target_rank]
+
+    # Power Iterations
+    if(powerIterations > 0):
+        for i in range(powerIterations):
+            Q = gram_schmidt(Y)
+            AQ = A.inner(Q)
+            Z, _ = spla.qr(AQ)
+            Y = A.lincomb(Z)
+
+    Q = gram_schmidt(Y)
+    B = Q.inner(A)
+
+    return Q, B
+
+
+@defaults('svd_method', 'distribution')
+def rand_dmd(A=None, XL=None, XR=None, target_rank=None, dt=1, modes='exact', svd_method='qr_svd', distribution='normal',
+             oversampling=0, powerIterations=0, order=True):
+    """
+    Ranzomized Dynamic Mode Decomposition
+
+    See Algorithm 4.1 in [EMKB19]_.
+
+
+    Parameters
+    ----------
+    A  :
+        The |VectorArray| for which the DMD Modes are to be computed.
+
+    target_rank : int
+        Number of DMD Modes to be computed. If None target_rank = len(A).
+
+    dt : scalar, optional (default: 1)
+        Factor specifying the time difference between the observations.
+        Used if the input data is a timeseries in continuous time.
+
+    modes : str `{'standard', 'exact', 'exact_scaled'}`
+        - 'standard' : uses the standard definition to compute the dynamic modes, `Wk = U * EVECS`.
+        - 'exact' : computes the exact dynamic modes, `Wk = XR * V * Sigma_inv * EVECS`.
+        - 'exact_scaled' : computes the exact dynamic modes,
+                           `Wk = (1/EVALS) * XR * V * Sigma_inv * EVECS`.
+
+    svd_method : str
+        Which SVD method from :mod:`~pymor.algorithms.svd_va` to use
+        (`'method_of_snapshots'` or `'qr_svd'`).
+
+    distribution : str
+        Distribution used for the randomized QB-Decomposition. (`'normal'` or `'uniform'`)
+
+    oversampling : int
+        Oversamplingparameter. Number of extra columns of the projectionmatrix.
+
+    powerIterations : int
+        Number of power Iterations.
+
+    order : bool `{True, False}`
+        True: return modes sorted.
+
+
+    Returns
+    -------
+    Wk :
+        |VectorArray| containing the dynamic modes.
+
+    omega :
+        Numpy Array containig time scaled eigenvalues: `ln(l)/dt`.
+
+    """
+
+    assert A is None or (XL is None and XR is None)
+    assert not(A is None and (XL and XR is None))
+    assert isinstance(A, VectorArray) or A is None
+    assert isinstance(XL, VectorArray) or XL is None
+    assert isinstance(XR, VectorArray) or XR is None
+    assert A is None or (target_rank is None or target_rank <= len(A))
+    assert XL is None or (target_rank is None or target_rank < len(XL))
+    assert XL is None and XR is None or len(XL) == len(XR)
+    assert distribution in ('normal', 'uniform')
+    assert modes in ('exact', 'standard', 'exact_scaled')
+    assert order in (True, False)
+    assert svd_method in ('qr_svd', 'method_of_snapshots')
+    assert oversampling >= 0
+    assert powerIterations >= 0
+
+    rank = len(A) if target_rank is None else target_rank
+
+    Q, B = rand_QB(A, target_rank=rank, distribution=distribution, oversampling=oversampling,
+                   powerIterations=powerIterations)
+
+    # transform B to VectorArray
+    B = NumpyVectorSpace.from_numpy(B.T)
+
+    Wk, omega = dmd(A=B, target_rank=None, dt=1, modes=modes, svd_method=svd_method, order=order)
+
+    # transform Wk to numpy
+    Wk = Wk.to_numpy()
+
+    Wk = Q.lincomb(Wk)
+
+    return Wk, omega
+
+
+def get_amplitudes(A, Wk):
+    '''Compute amplitueds b using least-squares: Fb=x1'''
+    W_OP = VectorArrayOperator(Wk)
+    return lsqr(W_OP, A[0])[0].to_numpy()[0]
+
+
+def get_vandermonde(A, EVals):
+    '''Compute Vandermonde matrix'''
+    return np.vander(EVals, N=len(A), increasing=True)
---
+++ parabolic_dmd.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pymor.algorithms.dmd import dmd, rand_dmd, get_amplitudes
+
+from pymor.analyticalproblems.instationary import InstationaryProblem
+from pymor.analyticalproblems.elliptic import StationaryProblem
+from pymor.analyticalproblems.functions import ExpressionFunction, ConstantFunction, LincombFunction
+from pymor.parameters.functionals import ProjectionParameterFunctional
+from pymor.analyticalproblems.domaindescriptions import RectDomain
+from pymor.discretizers.builtin.cg import discretize_instationary_cg
+from pymor.discretizers.builtin.grids.tria import TriaGrid
+
+problem = InstationaryProblem(
+
+    StationaryProblem(
+        domain=RectDomain(),
+
+        diffusion=ConstantFunction(0.01, dim_domain=2),
+
+        # advection=LincombFunction([ConstantFunction(np.array([-1., 0]), dim_domain=2)],
+        #                           [ProjectionParameterFunctional('speed')]),
+
+        reaction=ConstantFunction(0.5, dim_domain=2),
+
+        # rhs=ExpressionFunction('(x[..., 0] > 0.3) * (x[..., 0] < 0.7) * (x[..., 1] > 0.3)*(x[...,1]<0.7) * 0.',
+        #                        dim_domain=2),
+
+        dirichlet_data=ConstantFunction(value=0., dim_domain=2),
+    ),
+
+    T=1.,
+
+    initial_data=ExpressionFunction('(x[..., 0] > 0.3) * (x[..., 0] < 0.7) * (x[...,1]>0.3) * (x[..., 1] < 0.7) * 10.',
+                                    dim_domain=2),
+)
+
+print('Discretize ...')
+m, data = discretize_instationary_cg(
+    analytical_problem=problem,
+    grid_type=TriaGrid,
+    diameter=1. / 200.,
+    nt=50
+)
+grid = data['grid']
+print(grid)
+print()
+
+print('Solve ...')
+# U = m.solve({'speed': 0.3})
+U = m.solve()
+
+m.visualize(U, title='Solution of Parabolic Problem')
+
+# ----- Testing DMD -----
+
+W, E = dmd(A=U, modes='standard')
+W2, E2 = dmd(A=U, modes='exact')
+W3, E3 = dmd(A=U, modes='exact_scaled')
+W4, E4 = dmd(A=U, modes='standard', order=False)
+
+print('Visualize ...')
+m.visualize(W, title='DMD Modes - standard')
+plt.plot(E.real, E.imag, 'b.')
+plt.title('DMD Eigenvalues - standard')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+m.visualize(W4, title='DMD Modes - standard - not ordered')
+plt.plot(E4.real, E4.imag, 'b.')
+plt.title('DMD Eigenvalues - standard - not ordered')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+m.visualize(W2, title='DMD Modes - exact')
+plt.plot(E2.real, E2.imag, 'b.')
+plt.title('DMD Eigenvalues - exact')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+m.visualize(W3, title='DMD Modes - exact_scaled')
+plt.plot(E3.real, E3.imag, 'b.')
+plt.title('DMD Eigenvalues - exact_scaled')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+# ----- Testing rDMD -----
+rW, rE = rand_dmd(U, None, 1, 'standard', None, 'normal', oversampling=5, powerIterations=1)
+rW2, rE2 = rand_dmd(U, None, 1, 'standard', None, 'uniform', oversampling=10, powerIterations=2)
+rW3, rE3 = rand_dmd(U, 5, 1, 'exact', None, 'normal', oversampling=2, powerIterations=1)
+rW4, rE4 = rand_dmd(U, None, 1, 'exact', None, 'uniform', oversampling=2, powerIterations=1)
+
+print('Visualize ...')
+m.visualize(rW, title='randomized DMD Modes - standard - normal distribution')
+plt.plot(rE.real, rE.imag, 'b.')
+plt.plot(E.real, E.imag, 'rx')
+plt.title('rDMD Eigenvalues - standard - normal distribution')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+m.visualize(rW2, title='randomized DMD Modes - standard - uniform distribution')
+plt.plot(rE2.real, rE2.imag, 'b.')
+plt.plot(E.real, E.imag, 'rx')
+plt.title('rDMD Eigenvalues - standard - uniform distribution')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+m.visualize(rW3, title='randomized DMD Modes - exact - normal distribution')
+plt.plot(rE3.real, rE3.imag, 'b.')
+plt.plot(E2.real, E2.imag, 'rx')
+plt.title('rDMD Eigenvalues - exact - normal distribution')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
+
+m.visualize(rW4, title='randomized DMD Modes - exact - uniform distribution')
+plt.plot(rE4.real, rE4.imag, 'b.')
+plt.plot(E2.real, E2.imag, 'rx')
+plt.title('rDMD Eigenvalues - standard - uniform distribution')
+plt.xlabel('Real')
+plt.ylabel('Imaginary')
+plt.show()
